541faf6fc0a62b9e2a622679a429dd3335b064eb
Sagnik Das <sagnikdas53@live.com>
2021-12-16 00:14:46 +0530

    before adding router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
