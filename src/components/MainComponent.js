fd073ddb0e5603460e9d52d4beba9b31eb92659f
Sagnik Das <sagnikdas53@live.com>
2021-12-30 21:59:14 +0530

    trying to deploy

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,10 +8,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+
 
 const mapStateToProps = state => {
   return {
@@ -24,16 +21,6 @@ const mapStateToProps = state => {
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-    };
-  }
   render() {
     const HomePage = () => {
       return (
