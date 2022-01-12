adf5837399fdc818aa2a5869f1c0e81498a031f9
Sagnik Das <sagnikdas53@live.com>
2022-01-13 04:00:48 +0530

    partial completion of redux

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentFormComponent';
 
-function RenderDish( {dish} ) {
+function RenderDish({ dish }) {
   return (
     <Card>
       <CardImg top src={dish.image} alt={dish.name} />
@@ -15,7 +15,7 @@ function RenderDish( {dish} ) {
   );
 }
 
-function RenderComments( {comments} ) {
+function RenderComments({ comments, addComment, dishId }) {
 
   if (comments == null || comments.length === 0) {
     return (
@@ -27,7 +27,7 @@ function RenderComments( {comments} ) {
     return (
       <li key={comment.id}>
         <p>{comment.comment}</p>
-        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
       </li>
     );
   });
@@ -36,7 +36,7 @@ function RenderComments( {comments} ) {
     <div>
       <h4>Comments</h4>
       <ul className="list-unstyled">
-        { renderedComments }
+        {renderedComments}
       </ul>
     </div>
   );
@@ -63,12 +63,15 @@ const DishDetail = (props) => {
             <RenderDish dish={props.dish} />
           </div>
           <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.comments} />
-            <CommentForm />
+            <RenderComments comments={props.comments}
+              addComment={props.addComment}
+              dishId={props.dish.id}
+            />
+      <CommentForm dishId={dishId} addComment={addComment} />
           </div>
         </div>
-    </div>
-  );
+      </div>
+    );
   }
   else {
     return (
@@ -79,3 +82,6 @@ const DishDetail = (props) => {
 }
 
 export default DishDetail;
+
+//        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+// add this after the redux parts ahve been synced from remote.
